2f5cf60d9c3a8244f5d93f67926d754da607108a
Claudia <claudiainees@hotmail.com>
2026-05-30 21:46:53 -0300

    adding logs send email

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -11,12 +11,29 @@ const transporter = nodemailer.createTransport({
 async function enviarEmailAtivacao(email, token) {
     const link = `http://localhost:5173/ativar-conta?token=${token}`
 
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: 'Ativação da conta - Mapa de Eventos',
-        text: `Clique para ativar sua conta: ${link}`
-    })
+    try {
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Ativação da conta - Mapa de Eventos',
+            text: `Clique para ativar sua conta: ${link}`
+        })
+
+
+        transporter.verify((error, success) => {
+            if (error) {
+                console.log("SMTP ERRO:", error)
+            } else {
+                console.log("SMTP OK pronto pra enviar email")
+            }
+        })
+
+        console.log("EMAIL ENVIADO")
+
+    } catch (err) {
+        console.error("ERRO AO ENVIAR EMAIL:", err)
+    }
+
 }
 
 async function enviarEmailReset(email, token) {
