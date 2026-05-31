3b246f7e7570fd1a35abe49593d88707e61ef4c1
Claudia <claudiainees@hotmail.com>
2026-05-30 21:15:53 -0300

    fixing map view

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import MapView from '../components/MapView'
+import MapView from '../components/Mapview'
 
 const API_URL = `${import.meta.env.VITE_API_URL}`
 
