c9badef5e89e8cc712b30cf8ab57eb656308a3ab
Wagner Alexandre <68972860+WagnerAlexandre@users.noreply.github.com>
2026-06-17 01:44:55 -0300

    ♻️ Refatorando Home
    Novo estilo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,47 +1,59 @@
-import { useNavigate } from 'react-router-dom'
-import '../styles/home.css'
+import { useNavigate } from 'react-router-dom';
+import '../styles/home.css';
 
 function Home() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="home-container">
-      {/* Lado Esquerdo */}
+      
       <div className="home-left">
-        <h1 className="home-h1"> Este é o seu primeiro acesso? </h1>
-        <button 
-          className="home-btn btn-primary" 
-          onClick={() => navigate('/faq')}
-        >
-          Tutorial
-        </button>
-
+        <div className='title'>
+        <h1>Onde Fica? <br/></h1>
+        <img src="../src/assets/pinpoint.png" className="bg-icon" alt="" />
+        </div>
+        <p style={{ color: '#ffffff', fontSize: '1.2rem' }}>
+          Encontre aquele evento ou stand com facilidade!
+        </p>
       </div>
 
-      {/* Lado Direito */}
       <div className="home-right">
-        <h2 className="home-h2"> Já estive aqui antes. </h2>
+        <h2 style={{ marginBottom: '1rem' }}>Entrar no sistema</h2>
+        
         <button 
           className="home-btn btn-secondary" 
           onClick={() => navigate('/mapa')}
         >
-          Ver eventos
+          Ver Mapa de Eventos
         </button>
-                <button 
-          className="home-btn btn-outline" 
-          onClick={() => navigate('/cadastro')}
-          >
-          Criar Conta
-        </button>
+
         <button 
           className="home-btn btn-outline" 
           onClick={() => navigate('/login')}
         >
-          Logar
+          Fazer Login
+        </button>
+
+        <div style={{ marginTop: '2rem', textAlign: 'center' }}>
+          <p style={{ fontSize: '1.0rem', marginBottom: '10px'}}>Não tem conta?</p>
+          <button 
+            className="home-btn btn-primary" 
+            onClick={() => navigate('/cadastro')}
+          >
+            Criar conta gratuita
+          </button>
+        </div>
+
+        <button 
+          className="home-btn" 
+          style={{ background: 'none', border: 'none', textDecoration: 'underline', color: '#64748b' }}
+          onClick={() => navigate('/faq')}
+        >
+          Precisa de ajuda?
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
